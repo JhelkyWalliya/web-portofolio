5f9b18e0b594f2c7d5e25b2b984188ca96533b13
Jhelky W <100131477+JhelkyWalliya@users.noreply.github.com>
2025-04-22 11:15:01 +0700

    add project web portofolio
    Membagun web portofolio degan tailwindcss & React JS

---
+++ src/script.js
@@ -0,0 +1,21 @@
+ const roles = ["Jhelky Walliya","Web Developer", "UI/UX Designer"];
+    let currentIndex = 0;
+    const textElement = document.getElementById("animated-text");
+
+    function changeText() {
+        textElement.classList.add("opacity-0"); // mulai fade out
+
+        setTimeout(() => {
+            textElement.textContent = roles[currentIndex]; // ganti teks
+            textElement.classList.remove("opacity-0");     // fade in lagi
+            currentIndex = (currentIndex + 1) % roles.length; // loop ke teks berikutnya
+        }, 500);
+    }
+
+    changeText(); // tampilkan pertama kali
+    setInterval(changeText, 3000); // ganti tiap 3 detik
+
+
+
+
+
